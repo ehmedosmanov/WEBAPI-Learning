c80ddd0c2fd740268554e4eb2402e37a5a95aa5b
ehmedosmanov <ehmedosmanov@gmail.com>
2023-09-24 21:12:44 +0400

    Added project files

---
+++ PokemonReviewAPI/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
