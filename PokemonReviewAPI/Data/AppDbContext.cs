00918d105844d7535a39efc835c7e11ec40bc60c
ehmedosmanov <ehmedosmanov@gmail.com>
2023-10-01 00:18:24 +0400

    Created AppDbContext

---
+++ PokemonReviewAPI/Data/AppDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(p => new { p.PokemonId, p.CategoryId });
+
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(p => p.PokemonCategories)
+                .HasForeignKey(p => p.PokemonId);
+
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(p => p.PokemonCategories)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<PokemonOwner>()
+                .HasKey(p => new { p.PokemonId, p.OwnerId });
+
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(p => p.PokemonOwners)
+                .HasForeignKey(p => p.PokemonId); 
+
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(p => p.PokemonOwners)
+                .HasForeignKey(p => p.OwnerId);
+        }
+
+
+    }
+}
