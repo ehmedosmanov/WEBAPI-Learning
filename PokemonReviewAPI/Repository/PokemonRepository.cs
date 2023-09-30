01e8b3a9465bfd1d6d73aea7586de7bacabb673a
ehmedosmanov <ehmedosmanov@gmail.com>
2023-10-01 00:22:01 +0400

    Added PokemonRepository implementation

---
+++ PokemonReviewAPI/Repository/PokemonRepository.cs
@@ -0,0 +1,20 @@
+﻿using PokemonReviewAPI.Data;
+using PokemonReviewAPI.Interfaces;
+using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Repository
+{
+    public class PokemonRepository:IPokemonRepository
+    {
+        private readonly AppDbContext _db;
+        public PokemonRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _db.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
