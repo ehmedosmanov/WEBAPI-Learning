8ccf9f9ed2faecd6fa6219ab077bea2f27231bf8
ehmedosmanov <ehmedosmanov@gmail.com>
2023-10-01 00:19:25 +0400

    Created PokemonController

---
+++ PokemonReviewAPI/Controllers/PokemonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewAPI.Interfaces;
+using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType()]   
+    }
+}
